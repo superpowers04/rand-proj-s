b7bc26995ada6fadf20d3b8104148e56d8d5606e
Super <38338700+superpowers04@users.noreply.github.com>
2020-05-25 06:04:49 -0400

    Fix ~/local to use ~/.local

--- Hijackthisaltforunix.sh
+++ Hijackthisaltforunix.sh
@@ -19,8 +19,8 @@ echo "Please give us the link below when it generates"
     else
     	printf "\nInstalled applications (/usr/local):\n"
     	ls -1 /usr/share/applications
-    	printf "\nInstalled applications (~/local/share/applications):\n"
-    	ls -1 ~/local/share/applications
+    	printf "\nInstalled applications (~/.local/share/applications):\n"
+    	ls -1 ~/.local/share/applications
 	if [[ -f "/usr/bin/pacman" ]]; then
 		printf "\nInstalled applications (pacman):\n"
 		pacman -Q
