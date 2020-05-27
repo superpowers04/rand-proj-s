d9aeefac38252efb342a3f0516f4de6fcb6f472c
Super <38338700+superpowers04@users.noreply.github.com>
2020-05-27 07:03:12 -0400

    Create Minecraftserverinstaller.sh

---
+++ Minecraftserverinstaller.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+# if [[ "$term" == "" ]]; then
+# 	zenity --warning --text="You ran the script without terminal!"
+# 	exit
+# fi
+clear
+printf "Checking for java"
+java -version
+
+#strings
+#desktopfile="[Desktop Entry]\nName=Minecraft server\nComment=Opens your minecraft server\nExec=\nIcon=minecraft\nTerminal=false\nType=Application\nCategories=Game;" - Unset until it can be determined what terminals the person has
+
+
+
+
+
+if [[ "$?" == "127" ]]; then
+	javanotinstalled
+fi
+echo "Java detected"
+selectservertype
+
+
+
+
+
+
+
+function javanotinstalled {
+	clear
+	echo "Java not found, Would you like me to install it for you? (y|n)"
+	read answer
+	answer="${answer,,}"
+	if [[ "$answer" == "n" || "$answer" == "no" ]]; then
+		echo "Checking what package manager's you have installed"
+		installmessage="Installing Java using"
+		if [[ -f "/usr/bin/apt" ]]; then
+			echo "$installmessage apt"
+			sudo apt-get install openjdk-8-jre
+		elif [[ -f "/usr/bin/pacman" ]]; then
+			echo "$installmessage pacman"
+			sudo pacman -Syu jre8-openjdk
+		elif [[ -f "/usr/bin/yay" ]]; then
+			echo "$installmessage yay"
+			sudo yay jre8-openjdk
+		else
+			echo "Could not detect what package manager you are using, You will have to install it yourself"
+			read
+			exit
+		fi
+	elif [[ "$answer" == "n" || "$answer" == "no" ]]; then
+		echo "Java is required to run Minecraft servers, Exiting!"
+		read
+		exit
+	else
+		echo "invalid answer"
+		javanotinstalled
+	fi
+	echo "Done!"
+}
+
+function eulaview {
+	echo "By using this server software, you are agreeing"
+	echo "to Mojang's End User License agreement at:"
+	echo "https://account.mojang.com/documents/minecraft_eula"
+	echo "View the End User License Agreement? (y/n)"
+	read answer
+	answer="${answer,,}"
+	echo $answer
+	if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+		clear
+		echo "Opening the Eula in your browser"
+		xdg-open "https://account.mojang.com/documents/minecraft_eula"
+		eulaaccept
+	elif [[ "$answer" == "n" || "$answer" == "no" ]]; then
+		clear
+		eulaaccept
+	else
+		echo "invalid answer"
+		eulaview
+	fi
+}
+
+function eulaaccept {
+	echo "Do you agree to the EULA? (y/n) "
+	read answer
+	answer="${answer,,}"
+	if [[ "$answer" == "y" || "$answer" == "yes" ]]; then
+		echo "Bringing you to install page"
+	elif [[ "$answer" == "n" || "$answer" == "no" ]]; then
+	    echo "You must agree to the End User License Agreement to continue."
+	    echo "Installer will exit."
+	    read
+	    exit
+	else
+		echo "invalid answer"
+		eulaview
+	fi
+}
+
+function locationset {
+	echo "Where would you like to install the server"
+	echo "By default it will be installed at '/home/${USER}/minecraft-server/'"
+	echo "Press enter to use default"
+	read installpath
+	if [[ -e "$installpath" ]]; then
+		echo "install path valid"
+	elif [[ "$installpath" == "" ]];then
+		echo "installing to default location"
+		if [[ -e "$installpath" ]]; then
+			echo "install path exists already, adding '-1' to the end"
+			installpath="/home/${USER}/minecraft-server-1/"
+		else 
+			installpath="/home/${USER}/minecraft-server/"
+		fi
+		mkdir "$installpath"
+	else
+		echo "Invalid install path"
+		locationset
+	fi 
+}
+
+function selectservertype {
+	echo "Please choose the server software you would like.\n 1. Paper"
+	read answer 
+	servertype="${answer,,}"
+	if [[ "$servertype" == "paper" || "$servertype" == "1" ]]; then
+		eulaview
+		clear
+		locationset
+		paperinstall
+	else
+		echo "Invalid option"
+		selectservertype
+	fi
+
+}
+
+
+function paperinstall {
+	clear
+	cd "$installpath"
+	echo "$(basename "$0") will now download the"
+	echo "latest version of Paper server software on your device."
+	echo "Press any key to continue, Otherwise please close this window"
+	read
+	curl -o "./paper.jar" https://papermc.io/ci/job/Paper-1.15/lastSuccessfulBuild/artifact/paperclip.jar
+	executable="paper.jar"
+	echo "Installed to ${installpath}, Press enter to continue"
+	read
+	usualinstall
+}
+
+function usualinstall {
+	echo "eula=true" > ./eula.txt
+	echo "How much RAM (in megabytes) would you like to"
+	echo "allocate to the server?"
+	read ramallocationmb
+	case $ramallocationmb in
+	    ''|*[!0-9]*) ramallocationmb= "-Xmx${ramallocationmb}m" ;;
+	    *) ramallocationmb= "-Xmx1024m";;
+	esac
+	echo "Specify any additional JVM arguments you would like to add.\nPress enter to skip this step."
+	read additionaljavaargs
+	finished
+}
+function finished {
+	printf "cd \"$installpath\"\njava -jar $ramallocationmb $additionaljavaargs $executable" > ./start.sh
+	chmod +x ./start.sh
+	echo "All finished, Run the start.sh file located in $installpath to start your server,(From terminal just run 'cd \"${installpath}\"; ./server.sh')"
+	read
+	exit
+}
