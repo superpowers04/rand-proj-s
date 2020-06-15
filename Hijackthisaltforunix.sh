ca70f4a01153be00ef428f3f97020f86e6896317
Super <38338700+superpowers04@users.noreply.github.com>
2020-06-15 15:40:41 -0400

    Add usage and -f, Adjusted check to be a function

--- Hijackthisaltforunix.sh
+++ Hijackthisaltforunix.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 #echo "We need you to run this as root for diagnostics, If you do not understand what this means then just enter your system password below, It will not show the password for security reasons"
-echo "Analysing"
-{	
-	
+
+function check {
     printf "Hijackthis alternative for Unix using bash\nRun by:\n$USER \nUname:\n"
     uname -a
-	
+    
     printf "\nHost file\n"
     cat /etc/hosts
     if [[ -f "/private/etc/hosts" ]];then
-	    printf "\nSecond host file\n"
-	    cat /private/etc/hosts
-	  fi
+        printf "\nSecond host file\n"
+        cat /private/etc/hosts
+      fi
     printf "\nProcess List:\n"
     ps -ax
     if [[ -e /Applications ]];then
-    	printf "\nInstalled applications (Darwin):\n"
-    	ls -1 /Applications
+        printf "\nInstalled applications (Darwin):\n"
+        ls -1 /Applications
     else
-    	printf "\nInstalled applications (/usr/local):\n"
-    	ls -1 /usr/share/applications
-    	printf "\nInstalled applications (~/.local/share/applications):\n"
-    	ls -1 ~/.local/share/applications
-	if [[ -f "/usr/bin/pacman" ]]; then
-		printf "\nInstalled applications (pacman):\n"
-		pacman -Q
-	fi
-	if [[ -f "/usr/bin/apt" ]]; then
-		printf "\nInstalled applications (apt):\n"
-		apt list --installed
-	fi
+        printf "\nInstalled applications (/usr/local):\n"
+        ls -1 /usr/share/applications
+        printf "\nInstalled applications (~/.local/share/applications):\n"
+        ls -1 ~/.local/share/applications
+        if [[ -f "/usr/bin/pacman" ]]; then
+            printf "\nInstalled applications (pacman):\n"
+            pacman -Q
+        fi
+        if [[ -f "/usr/bin/apt" ]]; then
+            printf "\nInstalled applications (apt):\n"
+            apt list --installed
+        fi
     fi
+}
 
-} >> ~/hjtlog
-printf "\n We created a file at your home directory called 'hjtlog', Please send this to us"
+if [[ "$@" == "-h" ]]; then
+    printf "Usage:\n -h : Print this help message\n -f : Produce output file instead of uploading"
+elif [[ "$@" == "-f" ]]; then
+    echo "Analysing.."
+    check > ~/hjtlog
+    printf "\nWe created a file at your home directory called 'hjtlog', Please send this to us\n"
+else
+    echo "Analysing.."
+    check | curl -F 'sprunge=<-' http://sprunge.us/
+    echo "Please give us the link above"
+fi
